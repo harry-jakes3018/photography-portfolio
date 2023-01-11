f601b7be6aa8d3c1f1a45c60d4dd0da9b24854af
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2023-01-11 17:06:04 +0530

    Gallery Section Completed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Hero from "../components/Hero";
+import Slider from "../components/Slider";
+import { SliderData } from "../components/SliderData";
 
 export default function Home() {
     return (
@@ -20,6 +22,7 @@ export default function Home() {
                 heading="Capture Photography"
                 message="I capture moments in nature and keep them alive."
             />
+            <Slider slides={SliderData} />
         </>
     );
 }
